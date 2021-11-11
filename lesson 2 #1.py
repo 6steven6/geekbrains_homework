4292fd8a11d7e3cb6c370a78a447a6c2785d3cf9
Steven <Krutakcom1@gmail.com>
2021-11-11 22:53:29 +0300

    домашка ко второму уроку

---
+++ lesson 2 #1.py
@@ -0,0 +1,7 @@
+first = (type(15 * 3))
+second = (type(15 / 3))
+third = (type(15 // 2))
+fourth = (type(15 ** 2))
+
+result = f'1-ое выражение: {first} 2-ое выражение: {second} 3-ое выражение: {third} 4-ое выражение: {fourth}'
+print(result)
